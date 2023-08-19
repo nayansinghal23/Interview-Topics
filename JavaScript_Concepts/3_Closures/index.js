14018ba80327f2b5c6ed29a6cba1e0664112a258
Nayan <nayansinghal393@gmail.com>
2023-08-19 09:53:28 +0530

    Closures

---
+++ JavaScript_Concepts/3_Closures/index.js
@@ -0,0 +1,48 @@
+// Function along with its lexical scope is closure
+
+// function x() {
+//   var a = 7;
+//   return function y() {
+//     console.log(a);
+//   };
+//   a = 100;
+// }
+// let sum = (num1, num2) => {
+//   console.log(num1 + num2);
+//   x()();
+// };
+// sum(23, 67);
+
+// function x() {
+//   var a = 7;
+//   function y() {
+//     console.log(a);
+//   }
+//   a = 100;
+//   return y;
+// }
+// let sum = (num1, num2) => {
+//   console.log(num1 + num2);
+//   x()();
+// };
+// sum(23, 67);
+
+function z() {
+  function x() {
+    var a = 7;
+    function y() {
+      a = 100;
+      console.log(a);
+    }
+    a = 23;
+    console.log(a);
+    return y;
+  }
+  return x;
+}
+
+let sum = (num1, num2) => {
+  console.log(num1 + num2);
+  z()()();
+};
+sum(23, 67);
